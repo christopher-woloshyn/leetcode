bee4695afdf4f4db72d5f469b67eff8709ffa204
Christopher Woloshyn <cwolosh1@binghamton.edu>
2021-10-11 13:25:23 -0400

    Fixed small bug.

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -22,7 +22,7 @@ class Solution:
     This nested looping implies a time complexity of O(n^2).
     """
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
+        for i in range(len(nums) - 1):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
